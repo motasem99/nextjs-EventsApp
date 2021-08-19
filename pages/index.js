2cd9bbcbadbc3fe9a693abceed10e83f143b78bf
mutasem <motasemimad10@gmail.com>
2021-08-19 13:17:26 +0300

    configuring the head content

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/EventList';
 
 function HomePage({ events }) {
   return (
     <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta
+          name='description'
+          content=' Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
       <EventList items={events} />
     </div>
   );
