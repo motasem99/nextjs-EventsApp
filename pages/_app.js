20efd6c9eca9be1840dbad4e69429d8233060a5b
mutasem <motasemimad10@gmail.com>
2021-08-19 13:38:49 +0300

    working with the _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <head>
+        <meta name='viewport' content='initial-scale=1.0, width:device-width' />
+      </head>
       <Component {...pageProps} />
     </Layout>
   );
