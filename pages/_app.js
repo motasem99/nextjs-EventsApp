54592d90dda9ec2ee32a35bc8cfcd3a75118b830
mutasem <motasemimad10@gmail.com>
2021-08-19 13:43:34 +0300

    merging head content

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <head>
+        <title>Next Event </title>
+        <meta name='description' content='NextJs Events' />
         <meta name='viewport' content='initial-scale=1.0, width:device-width' />
       </head>
       <Component {...pageProps} />
