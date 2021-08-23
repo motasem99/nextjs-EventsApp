396a02e52c50260601db47ca5cd6f81fedfe4395
mutasem <motasemimad10@gmail.com>
2021-08-23 11:42:59 +0300

    Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+const { MongoClient } = require('mongodb');
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -6,6 +8,26 @@ function handler(req, res) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://mutasem-NextjsProject:2741999@cluster0.6sm4t.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
+
+    // const uri =
+    //   'mongodb+srv://mutasem-NextjsProject:2741999@cluster0.6sm4t.mongodb.net/newsletter?retryWrites=true&w=majority';
+    // const client = new MongoClient(uri, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+    // client.connect((err) => {
+    //   const collection = client.db().collection('emails');
+    //   // perform actions on the collection object
+    //   client.close();
+    // });
 
     console.log(userEmail);
     res.status(201).json({ message: 'Signed up!' });
