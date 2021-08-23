f11cbda2c297e5a70d08883981216b9c492d6f41
mutasem <motasemimad10@gmail.com>
2021-08-23 11:56:33 +0300

    inserting comments into the database

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,11 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect(
+    'mongodb+srv://mutasem-NextjsProject:2741999@cluster0.6sm4t.mongodb.net/events?retryWrites=true&w=majority'
+  );
 
   if (req.method === 'POST') {
     const { email, name, text } = req.body;
@@ -15,12 +21,18 @@ function handler(req, res) {
       return;
     }
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
-    console.log(newComment);
+
+    const db = client.db();
+    const result = await db.collection('comments').insertOne(newComment);
+
+    console.log(result);
+
+    newComment.id = result.insertedId;
 
     res.status(201).json({ message: 'Added comment', comment: newComment });
   }
@@ -32,6 +44,7 @@ function handler(req, res) {
     ];
     res.status(200).json({ comments: dummyList });
   }
+  client.close();
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -10,10 +10,10 @@ async function handler(req, res) {
     }
 
     const client = await MongoClient.connect(
-      'mongodb+srv://mutasem-NextjsProject:2741999@cluster0.6sm4t.mongodb.net/newsletter?retryWrites=true&w=majority'
+      'mongodb+srv://mutasem-NextjsProject:2741999@cluster0.6sm4t.mongodb.net/events?retryWrites=true&w=majority'
     );
     const db = client.db();
-    await db.collection('emails').insertOne({ email: userEmail });
+    await db.collection('newsletter').insertOne({ email: userEmail });
 
     client.close();
 
